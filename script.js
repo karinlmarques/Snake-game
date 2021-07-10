691f75d167dee49cf60fbc08a1b5fd4e51c2c879
karinlmarques <karinlmarques@gmail.com>
2021-07-10 19:10:04 -0300

    definir aleatoridade da frutinha e tamanho da cobrinha

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ snake[0] = {
 };
 let direction = "right";
 let food = {
-  x: Math.floor(Math.random()*15+1)*box,
-  y: Math.floor(Math.random()*15+1)*box
-}
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -24,8 +24,8 @@ function criarCobrinha() {
   }
 }
 
-function drawFood(){
-  context.fillStyle = 'red';
+function drawFood() {
+  context.fillStyle = "red";
   context.fillRect(food.x, food.y, box, box);
 }
 
@@ -55,7 +55,13 @@ function iniciarJogo() {
   if (direction == "left") snakeX -= box;
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
-  snake.pop();
+
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    (food.x = Math.floor(Math.random() * 15 + 1) * box),
+      (food.y = Math.floor(Math.random() * 15 + 1) * box);
+  }
 
   let newHead = {
     x: snakeX,
